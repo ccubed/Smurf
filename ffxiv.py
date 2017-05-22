28062c6e90ce3cbc5bf9ef6748966c0c26e1e708
Charles Click <CharlesClick@vertinext.com>
2017-05-22 00:43:51 -0400

    Work
    some initial work

---
+++ ffxiv.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+
+class Ffxiv():
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    #@commands.command()
+    #async def name(self, ctx, args):
---
+++ smurf.py
@@ -0,0 +1,51 @@
+import discord
+from discord.ext import commands
+
+startup_extensions = ['ffxiv']
+
+bot = commands.Bot(command_prefix=">", description="Smurf is an MMORPG Guild/Raid management focused Discord bot.")
+
+
+@bot.command()
+async def load(ctx, what: str):
+    """Load a Module"""
+    await ctx.message.edit(content="Loading {}...".format(what))
+    try:
+        bot.load_extension(what)
+        await ctx.message.edit(content="Loaded {}.".format(what))
+    except ImportError as e:
+        await ctx.message.delete()
+
+
+@bot.command()
+async def unload(ctx, what: str):
+    """Unload Modules"""
+    bot.unload_extension(what)
+    await ctx.message.delete()
+
+
+@bot.command()
+async def kill(ctx):
+    """Kill the Bot"""
+    await ctx.message.delete()
+    await bot.logout()
+
+
+@bot.command()
+async def status(ctx, what: str):
+    """Set status"""
+    await ctx.message.delete()
+    if bot.is_ready():
+        await bot.change_presence(game=discord.Game(name=what))
+
+
+@bot.event
+async def on_ready():
+    print("Bot Ready to process commands\nLogged in as: {}".format(bot.user.name))
+
+
+if __name__ == "__main__":
+    for ext in startup_extensions:
+        bot.load_extension(ext)
+
+    bot.run("")

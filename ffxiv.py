65536f16ae84df2227fbb8c3a47a94d412520a15
Charles Click <CharlesClick@vertinext.com>
2017-07-03 08:57:07 -0400

    Add fc details command

--- ffxiv.py
+++ ffxiv.py
@@ -1,3 +1,4 @@
+import json
 import discord
 from discord.ext import commands
 
@@ -14,20 +15,33 @@ class Ffxiv():
         pass
 
     @ff14.comamnd()
-    async def craft(self, what: str):
+    async def craft(self, ctx, what: str):
         """
         Built in crafting helper. Given an item, will tell you what materials are needed to craft it.
         """
         pass
 
     @ff14.command()
-    async def fc(self, name: str, server: str):
+    async def fc(self, ctx, name: str, server: str):
         """
         Set the name and server for the guild's FC.
         :param name: FC Name
         :param server: FC Server
         """
-        pass
+        jsd = {'name': name, 'server': server}
+        jsd = json.dumps(jsd)
+
+        async with self.bot.sql.acquire() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute("SELECT * FROM Guilds WHERE id = {}".format(ctx.guild.id))
+                if cur.rowcount:
+                    await cur.execute("UPDATE Guilds SET ffxiv = '{}' WHERE id = {}".format(jsd, ctx.guild.id))
+                    await conn.commit()
+                else:
+                    await cur.execute("INSERT INTO Guilds (id, ffxiv) VALUES ('{}', '{}')".format(ctx.guild.id, jsd))
+                    await conn.commit()
+
+        await ctx.send("Set your FC details.")
 
 
 def setup(bot):
